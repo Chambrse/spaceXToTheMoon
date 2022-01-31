313f458e868ffcb21089db5b2042c2115a367910
Shane Chambry <shaneechambry@gmail.com>
2022-01-30 22:10:59 -0500

    new bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
+
 module.exports = {
-output: {
-    filename: 'bundle.js',
-    path: '/dist',
-  }
-};
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'static'),
+      filename: 'bundle.js',
+    },
+  };
